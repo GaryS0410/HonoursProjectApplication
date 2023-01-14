bb50d4c20323eb3c76f5b47d85f3740274be5e65
GaryS0410 <garyjsmith0410@gmail.com>
2023-01-14 19:18:35 +0000

    Added ability to stop session

--- app/static/js/camera.js
+++ app/static/js/camera.js
@@ -2,19 +2,21 @@ let canvas = document.querySelector('#canvas');
 let context = canvas.getContext('2d');
 let video = document.querySelector('#video');
 
+var intervalId;
+
 if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
     navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
         video.srcObject = stream;
         video.play();
     })
-}
+} 
 
 function upload(file){
     var formdata = new FormData();
     formdata.append('snap', file);
 
     var xhr = new XMLHttpRequest();
-    xhr.open('POST', 'http://127.0.0.1:5000/saveImage', true);
+    xhr.open('POST', '/', true);
     xhr.onload = function() {
         if (this.status == 200) {
             console.log(this.response);
@@ -32,5 +34,10 @@ function takePhoto() {
 }
 
 document.getElementById('snap').addEventListener('click', () => {
-    setInterval(takePhoto, 1500)
+    intervalId = setInterval(takePhoto, 3000); 
+})
+
+document.getElementById('stop').addEventListener('click', () => {
+    clearInterval(intervalId);
+    alert('Session Stopped');
 })
--- app/views.py
+++ app/views.py
@@ -1,31 +1,66 @@
 from app import app
 from flask import Flask, render_template, request, Response, jsonify, make_response
 import numpy as np
+from PIL import Image
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.utils import load_img
 import os
 from werkzeug.utils import secure_filename
 
+
 img_counter = 0
+image_list = np.array([])
+model = load_model('app\models\initalModel.h5')
 
-@app.route('/')
+@app.route('/', methods = ['GET', 'POST'])
 def index():
-    return render_template('index.html')
-
-@app.route('/saveImage', methods = ['GET', 'POST'])
-def uploadImage():
-    global img_counter
     if request.method == 'POST':
         fs = request.files.get('snap')
         if fs:
-            print('FileStorage', fs)
-            print('Filename', fs.filename)
-            fs.save('uploaded_images/image_{}.jpeg'.format(img_counter))
-            img_counter += 1
+            global image_list
+            image = Image.open(fs)
+            image_list = np.append(image_list, image)
+            print(len(image_list))
             return 'got photo'
         else:
             return 'no photo'
+    return render_template('index.html')
+
+def read_image(filename):        
+    img = load_img(filename, grayscale = True, target_size = (48, 48), keep_aspect_ratio = True)
+    x = image.img_to_array(img)
+
+    x = np.expand_dims(x, axis = 0)
+    x = x.reshape(1, 48, 48, 1)
+
+    x /= 255.0
+
+    x = x.astype('float32')
+    x = x.reshape(1, 48, 48, 1)
+    return x
+
+@app.route('/predict', methods = ['GET'])
+def predict_emotion():
+    if request.method == 'GET':
+        image_list = read_image(image_list)
+        class_prediction = model.predict(image_list)
+        classes_x = np.argmax(class_prediction, axis = 1)
+        if classes_x == 0:
+            emotion = 'angry'
+        elif classes_x == 1:
+            emotion = 'disgust'
+        elif classes_x == 2:
+            emotion = 'fear'
+        elif classes_x == 3:
+            emotion = 'happy'
+        elif classes_x == 4:
+            emotion = 'sad'
+        elif classes_x == 5:
+            emotion = 'surprise'
+        elif classes_x == 6:
+            emotion = 'neutral'
+        return render_template('index.html', emotion = emotion, prob = class_prediction)
 
 @app.route('/about')
 def about():
