dfc51ba4a6df03b8e6d3124438842b90ec018054
GaryS0410 <garyjsmith0410@gmail.com>
2023-02-05 22:54:25 +0000

    Continued work on user system, ability to register account, save user data, and display it through HTML

--- app/models.py
+++ app/models.py
@@ -1,11 +1,20 @@
 from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
+from sqlalchemy.types import PickleType
+import json
+
+class EmotionData(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    emotion_type = db.Column(db.String(20), nullable=False)
+    emotion_instances = db.Column(db.Integer, nullable=False)
+    session_id = db.Column(db.Integer, db.ForeignKey('session_data.id'))
 
 class SessionData(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    time_of_session = db.Column(db.DateTime(timezone=True), default=func.now())
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
+    emotion_data = db.relationship('EmotionData')
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key = True)
--- app/views.py
+++ app/views.py
@@ -4,7 +4,10 @@ from PIL import Image
 from tensorflow.keras.models import load_model
 import cv2
 from io import BytesIO
+
 from flask_login import login_required, current_user
+from . import db
+from .models import SessionData, EmotionData
 
 # Views.py blueprint declaration
 views = Blueprint('views', __name__)
@@ -49,7 +52,7 @@ def preprocessImage(webcamImage):
 @views.route('/')
 @login_required
 def index():
-    return render_template('index.html')
+    return render_template('index.html', user=current_user)
 
 @views.route('/startSession', methods = ['GET', 'POST'])
 def startSession():
@@ -84,11 +87,34 @@ def predict_emotion():
             else: 
                 emotions_count[i] = 1 
 
+        session_db = SessionData(user_id = current_user.id)
+        db.session.add(session_db)
+        db.session.commit()
+
+        for emotion_name, count in emotions_count.items():
+            emotion_db = EmotionData(emotion_type = emotion_name, emotion_instances=count, session_id = session_db.id)
+            db.session.add(emotion_db)
+        db.session.commit()
+
         print(emotions_count)
 
         image_list = np.zeros((1, 48, 48, 1))
         return jsonify(emotions_count)
 
+# @views.route('/previous')
+# @login_required
+# def displayPreviousData():
+#     sessions = SessionData.query.filter_by(user_id = current_user.id).all()
+#     return render_template('previousSessions.html', sessions = sessions)
+
 @views.route('/previous')
-def about():
-    return render_template('previousSessions.html')
+@login_required
+def displayPreviousData():
+    sessions = SessionData.query.filter_by(user_id = current_user.id).all()
+    session_data = []
+    for session in sessions:
+        emotions_count = {}
+        for emotion in session.emotion_data:
+            emotions_count[emotion.emotion_type] = emotion.emotion_instances
+        session_data.append({"session_id": session.id, "emotions_count": emotions_count})
+    return render_template('previousSessions.html', sessions = sessions , session_data=session_data)
