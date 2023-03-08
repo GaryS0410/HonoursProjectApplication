b336ad3c44a6283ddc0129f00494b5506eca8170
GaryS0410 <garyjsmith0410@gmail.com>
2023-03-08 19:16:25 +0000

    Some work on re-factoring user database schema. Can't figure it out for now. May come back and try to achieve it later on

--- app/models.py
+++ app/models.py
@@ -4,8 +4,8 @@ from sqlalchemy.sql import func
 
 class Association(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    patient_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    therapist_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    patient_id = db.Column(db.Integer, db.ForeignKey('patient.id'))
+    therapist_id = db.Column(db.Integer, db.ForeignKey('therapist.id'))
 
 class EmotionData(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -21,15 +21,35 @@ class SessionData(db.Model):
     emotion_data = db.relationship('EmotionData')
 
 class User(db.Model, UserMixin):
-    # User attributes
-    id = db.Column(db.Integer, primary_key = True)
+    __tablename = 'user'
+    id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(50))
     surname = db.Column(db.String(50))
     email = db.Column(db.String(50), unique=True)
     password = db.Column(db.String(150))
     is_therapist = db.Column(db.Boolean)
-    phq9_score = db.Column(db.Integer, default=0)
-    # User relationships
+
+    __mapper_args__ = {
+        'polymorphic_identity': 'user',
+        'polymorphic_on': is_therapist
+    }
+
+class Patient(User):
+    __tablename__ = 'patient'
+    id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+    phq9_score = db.Column(db.Integer)
     session_data = db.relationship('SessionData')
-    patients = db.relationship('Association', foreign_keys = [Association.therapist_id], backref='therapist')
-    therapist = db.relationship('Association', foreign_keys=[Association.patient_id], backref='patient', uselist=False)
+    therapist = db.relationship('Association', foreign_keys = [Association.therapist_id], backref='therapist')
+
+    __mapper_args__ = {
+        'polymorphic_identity': 'patient'
+    }
+
+class Therapist(User):
+    __tablename__ = 'therapist'
+    id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+    patients = db.relationship('Association', foreign_keys=[Association.patient_id], backref='patient', uselist=False)
+
+    __mapper_args__ = {
+        'polymorphic_identity': 'therapist'
+    }
